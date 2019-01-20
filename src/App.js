3e7a5595ce05fd5e4b96677e2e75902cccdb09f6
Cam Prier <CSPrier@gmail.com>
2019-01-20 13:06:54 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+ constructor(props) {
+  super(props);
+  this.state = {
+    items: [
+        {
+          name: 'item1',
+          itemId: 1,
+          checked: false
+        }
+      ],
+      newItemName: ''
+    };
+    this.addItem = this.addItem.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.toggleChecked = this.toggleChecked.bind(this);
+  };
+
+  handleChange(e) {
+    this.setState({
+      newItemName: e.target.value
+    });
+  };
+
+  addItem(e) {
+    let nextIndex = this.state.items[this.state.items.length - 1].itemId + 1;
+    let newItem = {
+      name: this.state.newItemName,
+      itemId: nextIndex,
+      checked: false
+    };
+    this.setState(prevState => ({
+      items: [...prevState.items, newItem]
+    }));
+    e.preventDefault();
+    this.refs.form.reset();
+  };
+
+  toggleChecked(itemId) {
+    console.log('ToggleChecked ItemID', itemId);
+    let targetItem = this.state.items.filter(item => item.itemId === itemId)[0]
+    targetItem.checked = !targetItem.checked;
+    this.setState({
+      items: this.state.items.map(item => (item.itemId === itemId) ? targetItem : item)
+    });
+  }
+
   render() {
-    return (
+    console.log('RenderState:', this.state.items);
+    const listAllItems = this.state.items.map((item, i) => {
+      return (
+        <li key={i}>
+          <p className={(item.checked) ? 'active' : 'non-active'}>{item.name}</p>
+          <button onClick={() => this.toggleChecked(item.itemId)}>Toggle Checked</button>
+        </li>
+      )
+    });
+
+    return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>List of Items</h1>
+        <form ref="form" onSubmit={this.addItem}>
+          <input type="text" name="addItems" value={this.state.newItemName} onChange={this.handleChange}/>
+          <button type="submit">Add an item</button>  
+        </form>
+        <ul>
+          {listAllItems}
+        </ul>
       </div>
-    );
+    )
   }
 }
-
+ 
 export default App;
